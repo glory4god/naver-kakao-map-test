8bc1f3bde5b31d3988e3bdf08d32b820162de3de
Hayoung <gkduddy0814@gmail.com>
2024-02-09 23:16:41 +0900

    fix: 점지도 추가

--- src/pages/apt/[buildingId].tsx
+++ src/pages/apt/[buildingId].tsx
@@ -1,11 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 
 export default function ChapterPage({}) {
   const router = useRouter();
 
+  const mapRef = useRef<naver.maps.Map | null>(null);
+
   useEffect(() => {
     let map: naver.maps.Map;
+
+    function startDotMap(data?: any) {
+      var dotmap = new naver.maps.visualization.DotMap({
+        map: map,
+        data: [[37.3595704, 127.105399]],
+      });
+    }
 
     const initMap = () => {
       const center: naver.maps.LatLng = new naver.maps.LatLng(
@@ -17,11 +26,18 @@ export default function ChapterPage({}) {
         center: center,
         zoom: 16,
       });
+      mapRef.current = map;
+
+      naver.maps.Event.once(map, 'init', () => {
+        startDotMap();
+      });
     };
+
     initMap();
 
     return () => {
       map.destroy();
+      mapRef.current = null;
     };
   }, []);
 
